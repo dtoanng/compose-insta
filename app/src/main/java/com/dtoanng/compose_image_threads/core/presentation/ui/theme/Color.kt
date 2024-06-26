f49829bf280bacdb600536b54e9cc27219862172
Nguyen Duy Toan <duytoan.ng1@gmail.com>
2024-06-26 15:54:33 +0700

    - chore: add resource files

--- app/src/main/java/com/dtoanng/compose_image_threads/core/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/dtoanng/compose_image_threads/core/presentation/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val AccentColor = Color(0xFF3797EF)
+
+val FormFieldBorderLight = Color(0x1A000000)
+val FormFieldBorderDark = Color(0x33FFFFFF)
+val FormFieldBgDark = Color(0xFF121212)
+val FormFieldBgLight = Color(0xFFFAFAFA)
+
+val IconDark = Color(0xFFF9F9F9)
+val IconLight = Color(0xFF262626)
+
+val LightBack = Color.Black.copy(alpha = 0.7f)
+val LightGray = Color.Gray.copy(alpha = 0.7f)
+val LineGrayColor = Color.Gray.copy(alpha = 0.2f)
+val LineBlueColor = Color.Blue.copy(alpha = 0.2f)
